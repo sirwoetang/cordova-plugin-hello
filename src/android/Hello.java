6787eedcfe78788ff129fe8a27b5b8a07c979a4f
Waldo PC <wboshoff@viewmetrics.co.za>
2015-09-10 22:30:46 +0200

    adding first plugin

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -4,12 +4,9 @@ import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 import android.app.Activity;
+import android.app.Activity.*;
 import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-
+import android.content.Intent.*;
 
 public class Hello extends CordovaPlugin {
 
