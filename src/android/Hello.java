22367bd3dc7c84e067f08950540d8a4429fd8f9d
Waldo PC <wboshoff@viewmetrics.co.za>
2015-09-11 00:15:16 +0200

    adding first plugin

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -3,34 +3,21 @@ package com.mapworxxell.onefourone;
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
-import android.content.Intent;
 
 public class Hello extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
-        if (action.equals("record")) {
+        if (action.equals("greet")) {
 
             String name = data.getString(0);
             String message = "Hello, " + name;
             callbackContext.success(message);
-            //Intent intent = new Intent("com.zello.ptt.down");
-            //intent.putExtra("com.zello.stayHidden", true);
-            //((CordovaActivity) this.cordova.getActivity()).sendBroadcast(intent);
-
 
             return true;
 
         } else {
-            String name = data.getString(0);
-            String message = "STOP, " + name;
-            callbackContext.success(message);
-
-            //Intent intent = new Intent("com.zello.ptt.up");
-            //intent.putExtra("com.zello.stayHidden", true);
-            //((CordovaActivity) this.cordova.getActivity()).sendBroadcast(intent);
-
 
             return false;
 
